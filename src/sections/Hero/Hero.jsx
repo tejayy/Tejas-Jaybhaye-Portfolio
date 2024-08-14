8c4fe2427f9ea781cc5f4ecd0027112abb753dda
tejayy <tbj456@gmail.com>
2024-08-14 23:55:15 +0530

    added assets and hero component

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,48 @@
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/hero-img.png";
+import themeIcon from "../../assets/sun.svg";
+import twitterIcon from "../../assets/twitter-light.svg";
+import githubIcon from "../../assets/github-light.svg";
+import linkedinIcon from "../../assets/linkedin-light.svg";
+import CV from "../../assets/Tejas Jaybhaye.pdf";
+
+function Hero() {
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={heroImg}
+          alt="profile picture of Tejas"
+        />
+        <img className={styles.colorMode} src={themeIcon} alt="colorModeIcon" />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Tejas <br /> Jaybhaye
+        </h1>
+        <h2>Frontend Developer</h2>
+        <span>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/tejayy" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/tejasjaybhaye/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
