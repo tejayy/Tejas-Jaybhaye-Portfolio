19e55c42f0b2680666f699d680fe50d906fe0248
tejayy <tbj456@gmail.com>
2024-08-15 00:17:48 +0530

    added theme component

---
+++ src/common/ThemeContext.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { createContext } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem("theme") || "light"
+  );
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+    localStorage.getItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
